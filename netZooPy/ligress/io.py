82b2c1342fc9da01a92076bc382569383cb2d6ee
Enakshi Saha <66750297+Enakshi-Saha@users.noreply.github.com>
2026-03-25 15:13:13 -0400

    Fix prepare_expression to correctly read index column when usecols is specified

--- netZooPy/ligress/io.py
+++ netZooPy/ligress/io.py
@@ -214,9 +214,11 @@ def prepare_expression(expression_filename, samples = None):
     # a list of sample of interest is passed
     print(samples)
     if type(expression_filename) is str:
-        
+        # get the name of the first column (gene index column)
+        first_col = pd.read_csv(expression_filename, sep='\t', nrows=0).columns[0]
+
         if (isinstance(samples, list)):
-            expression_data = read_expression(expression_filename, usecols = samples)
+            expression_data = read_expression(expression_filename, usecols=[first_col] + list(samples))
         else:
             expression_data = read_expression(expression_filename)
             
